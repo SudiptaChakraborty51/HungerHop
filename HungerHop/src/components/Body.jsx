0907c7f3e2cc9f46eefb91060906118e5b1e180f
Sudipta <hisudipta26@gmail.com>
2025-07-10 19:35:32 +0530

    feat: added search functionality

--- HungerHop/src/components/Body.jsx
+++ HungerHop/src/components/Body.jsx
@@ -5,21 +5,45 @@ import { useEffect, useState } from "react";
 const Body = () => {
   const [isTopRatedClicked, setIsTopRatedClicked] = useState(false);
   const [restaurantsData, setRestaurantsData] = useState(restaurants);
+  const [searchedRestaurant, setSearchedRestaurant] = useState("");
 
   useEffect(() => {
-    if(isTopRatedClicked) {
-        const updatedRestaurants = restaurantsData?.filter(item => item?.info?.avgRating >= 4);
-        setRestaurantsData(updatedRestaurants);
+    if (isTopRatedClicked) {
+      const updatedRestaurants = restaurantsData?.filter(
+        (item) => item?.info?.avgRating >= 4
+      );
+      setRestaurantsData(updatedRestaurants);
     } else {
-        setRestaurantsData(restaurants);
+      setRestaurantsData(restaurants);
     }
   }, [isTopRatedClicked]);
+
+  useEffect(() => {
+    if(!searchedRestaurant.trim()) {
+      setRestaurantsData(restaurants);
+    } else {
+      const filteredList = restaurantsData?.filter((item) =>
+        item.info.name.toLowerCase().includes(searchedRestaurant.toLowerCase())
+      );
+      setRestaurantsData(filteredList);
+    }
+  }, [searchedRestaurant]);
 
   return (
     <div className="body">
       <div className="search-bar">
-        <input className="search-input" placeholder="Search" />
-        <button className="top-rated-btn" onClick={() => setIsTopRatedClicked(prev => !prev)}>Top Rated</button>
+        <input
+          className="search-input"
+          placeholder="Search"
+          value={searchedRestaurant}
+          onChange={(e) => setSearchedRestaurant(e.target.value)}
+        />
+        <button
+          className="top-rated-btn"
+          onClick={() => setIsTopRatedClicked((prev) => !prev)}
+        >
+          Top Rated
+        </button>
       </div>
       <div className="restaurant-container">
         {restaurantsData?.map((res) => (
