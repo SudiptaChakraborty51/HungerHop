ed3573e5ae8d8efeca5158b4652f4bdb4a66d72b
Sudipta <hisudipta26@gmail.com>
2025-07-10 19:21:04 +0530

    feat: filtered top rated restaurants

--- HungerHop/src/components/Body.jsx
+++ HungerHop/src/components/Body.jsx
@@ -1,14 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../assets/constants";
+import { useEffect, useState } from "react";
 
 const Body = () => {
+  const [isTopRatedClicked, setIsTopRatedClicked] = useState(false);
+  const [restaurantsData, setRestaurantsData] = useState(restaurants);
+
+  useEffect(() => {
+    if(isTopRatedClicked) {
+        const updatedRestaurants = restaurantsData?.filter(item => item?.info?.avgRating >= 4);
+        setRestaurantsData(updatedRestaurants);
+    } else {
+        setRestaurantsData(restaurants);
+    }
+  }, [isTopRatedClicked]);
+
   return (
     <div className="body">
       <div className="search-bar">
         <input className="search-input" placeholder="Search" />
+        <button className="top-rated-btn" onClick={() => setIsTopRatedClicked(prev => !prev)}>Top Rated</button>
       </div>
       <div className="restaurant-container">
-        {restaurants?.map((res) => (
+        {restaurantsData?.map((res) => (
           <RestaurantCard key={res?.info?.id} restaurant={res} />
         ))}
       </div>
